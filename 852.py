389d8ce0cb23e8607b7127a4bf60fadc74c3bdb9
James Hageman <jamesd@hageman.ca>
2018-07-29 12:01:58 -0700

    working 852.py

---
+++ 852.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, A):
+        start = 0
+        end = len(A) - 1
+        m = (start + end) // 2
+        while not (A[m - 1] < A[m] and A[m] > A[m + 1]):
+            if A[m - 1] < A[m]:
+                start = m
+            else:
+                end = m
+            m = (start + end) // 2
+        return m
+
+
+s = Solution()
+print(s.peakIndexInMountainArray([3, 4, 5, 4]))
