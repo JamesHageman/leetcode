16b06e1f226e671d9fe8afc39ce40520abec75f2
James Hageman <jamesd@hageman.ca>
2018-07-29 12:35:46 -0700

    working 214.py

--- 214.py
+++ 214.py
@@ -1,47 +1,13 @@
 class Solution:
     def shortestPalindrome(self, s):
-        A = self.palindromesByLength(s)
-        A = [edge_ps(ps, s) for ps in A]
-        A = [ps for ps in A if ps]
-        i, j, edge_p = A[-1][0]
-        return reverse(s[j:]) + s
+        i = 0
+        for i in range(len(s), 0, -1):
+            p = s[:i]
+            if p == p[::-1]:
+                break
 
-    def palindromesByLength(self, s):
-        A = [
-            [(0, 0, "")],
-            [(i, i + 1, s[i : i + 1]) for i in range(len(s))],
-            [(i, i + 2, s[i : i + 2]) for i in range(len(s) - 1) if s[i] == s[i + 1]],
-        ]
-        for size in range(3, len(s) + 1):
-            new_palindromes = [
-                (i - 1, j + 1, s[i - 1 : j + 1])
-                for i, j, _ in A[size - 2]
-                if is_palindrome(s[i - 1 : j + 1]) and i > 0 and j < len(s)
-            ]
-            A.append(new_palindromes)
+        return s[i:][::-1] + s
 
-        return [ps for ps in A if ps]
-
-
-def is_palindrome(s):
-    return s == s[::-1]
-
-
-def edge_ps(ps, s):
-    return [p for p in ps if p[0] == 0]
-
-
-def reverse(s):
-    return s[::-1]
-
-
-"""
-
- i
- ^
-babad
-
-"""
 
 if __name__ == "__main__":
     import sys
