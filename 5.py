d755270093f395415b720aa4429f9fc550d0cbc5
James Hageman <jamesd@hageman.ca>
2018-07-29 12:02:07 -0700

    broken 5.py

---
+++ 5.py
@@ -0,0 +1,45 @@
+class Solution:
+    def longestPalindromeSlow(self, s):
+        # brute force :(
+        x = ""
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                p = s[i:j]
+                if p == p[::-1] and len(p) > len(x):
+                    x = p
+        return x
+
+    def longestPalindrome(self, s):
+        A = [
+            [(0, 0, "")],
+            [(i, i + 1, s[i : i + 1]) for i in range(len(s))],
+            [(i, i + 2, s[i : i + 2]) for i in range(len(s) - 1) if s[i] == s[i + 1]],
+        ]
+        for size in range(3, len(s) + 1):
+            new_palindromes = [
+                (i - 1, j + 1, s[i - 1 : j + 1])
+                for i, j, _ in A[size - 2]
+                if is_palindrome(s[i - 1 : j + 1]) and i > 0 and j < len(s)
+            ]
+            A.append(new_palindromes)
+
+        return [ps for ps in A if ps][-1][0][2]
+
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+
+"""
+
+ i
+ ^
+babad
+
+"""
+
+if __name__ == "__main__":
+    import sys
+
+    s = Solution()
+    print(s.longestPalindrome(sys.argv[1]))
